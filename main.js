9d0518c53c7008cfbf2623ae102b5156e5bc6391
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-12 12:48:51 -0500

    feat(Taller2-Javascript): :rocket: Ejercicio 9
    Solucion al ejercicio 9 del segundo  taller de javascript. ingresamos x atletas y calculamos cual fue el ganador y si rompio algun record
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-/* EJERCICOIO 8--.Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo */
+/* EJERCICOIO 9--.N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros. */
 do {
-    let option = Number(prompt(`Digite la opcion que deseas realizar\n1.Perimetro de un cuadrado\n2.Area del rectangulo`))
+    data_athletes = {
+        name:[],
+        marca:[]
+    };
+    let record = 15.50;
 
-    if(option > 0 && option <= 2) {
-        if(option === 1){
-            let side_square = Number(prompt(`Digite un lado del cuadrado (cm)`))
-            alert(`El perimetro del cuadrado es ${side_square * 4}cm`)
-        } else{
-            let base_rectangulo = Number(prompt(`Digite la base del rectangulo (cm)`));
-            let heigth_rectangulo = Number(prompt(`Digite la altura del rectangulo (cm)`))
-            alert(`El area del rectangulo es ${base_rectangulo * heigth_rectangulo}cm`)
+    let cant_athletes = Number(prompt(`Digite cuantos atletas pasaron a la final`));
+    save_athletes(cant_athletes);
+    alert(`La campeona, medalla de oro de los juegos olimpicos en la modalidad salto triple es ${calculate_winner(data_athletes)}`)
+
+    function save_athletes(cantidad) {
+        for (let i = 1; i <= cantidad; i++) {
+            let name_athlete = prompt(`Digite el nombre del atleta ${i}`);
+            let marca_athletes = Number(prompt(`Digite la marca del salto (m)`));
+
+            data_athletes.name.push(name_athlete.toUpperCase());
+            data_athletes.marca.push(marca_athletes); 
+
+            if(marca_athletes > record){
+                alert(`Felicitaciones!!! ${name_athlete} acabas de romper el record anterior de ${record}m, te acaba de ganas 500 millones`);
+                record = marca_athletes
+            }          
         }
-    }else{
-        alert(`Error opcion incorrecta`)
+    }
+    function calculate_winner(data) {
+        let marca_winner = Math.max(...data.marca);
+        let position_winner = data.marca.indexOf(marca_winner);
+        
+        return `${data.name[position_winner]} con una marca de ${marca_winner}m`
     }
  } while (confirm("¿Desea ingresar otra opcion?"));
