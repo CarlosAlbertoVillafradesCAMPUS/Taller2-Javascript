f17fbcbbfb834e9cad518341fdbedacf61c20eb2
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-12 12:07:33 -0500

    feat(Taller2-Javascript): :rocket: Ejercicio 7
    Solucion al ejercicio 7 del segundo  taller de javascript. mostramos una factura con el nombre, el precio y la cantidad de un articulo, calculando el valor totl a pagar por el cliente
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,41 +1,10 @@
-/* EJERCICOIO 6--.Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres. */
+/* EJERCICOIO 6--.Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura. */
 do {
-  let data_students = {
-    name: [],
-    sexo: [],
-    note: [],
-  };
-  let cant_men = 0;
-  let cant_women = 0;
+  let name_article = prompt(`Digite el nombre del articulo`);
+  let price_article = Number(prompt(`Digite el precio del articulo (${name_article})`));
+  let cant_article = Number(prompt(`Digite la cantidad de ${name_article} que va a llevar`))
 
-  let cant_students = Number(prompt(`Digite la cantidad de estudiantes que va a ingresar`));
-  save_students(cant_students);
-  alert(`El estudiantes con la MAYOR nota definitva es ${MaxNote_MinNote(data_students)}\n
-De los ${cant_students} estudiantes\n${cant_men} son hombres.\n${cant_women} son mujeres`);
-  
-  function save_students(estudiantes) {
-    for (let i = 1; i <= estudiantes; i++) {
-      name_student = prompt(`Digite el nombre del estudinate ${i}`);
-      sexo_student = Number(prompt(`Digite el sexo del estudiante ${i}\n1.Hombre\n2.Mujer`));
-      note_student = Number(prompt(`Digite la nota final del estudiante ${i}`));
-
-      data_students.name.push(name_student);
-      data_students.note.push(note_student);
-
-      (sexo_student === 1) ? (cant_men += 1) :(cant_women += 1)
-    }
-  }
-
-  function MaxNote_MinNote(data) {
-    let max_note = Math.max(...data.note);
-    let min_note = Math.min(...data.note);
-    let position_max_note = data.note.indexOf(max_note);
-    let position_min_note = data.note.indexOf(min_note);
-
-    return `${data.name[position_max_note]} con ${data.note[position_max_note]}\nEl estudiantes con la MENOR nota definitva es ${data.name[position_min_note]} con ${data.note[position_min_note]}`;
-  }
-} while (confirm("¿Desea ingresar otros estudiantes?"));
+  alert(`Nombre del articulo: ${name_article}\nPrecio Articulo: ${price_article}\nCantidad a Llevar: ${cant_article}\n\nTotal a Pagar: ${price_article*cant_article}`)
+} while (confirm("¿Desea ingresar otro Articulo?"));
