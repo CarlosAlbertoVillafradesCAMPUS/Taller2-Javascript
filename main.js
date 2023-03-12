08360701eb205c3d75a67b5c8918b714450b4d49
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-12 11:57:23 -0500

    feat(Taller2-Javascript): :rocket: Ejercicio 6
    Solucion al ejercicio 6 del segundo  taller de javascript. se ingresaban x estudiantes y calculamos el estudiante con la nota mayor, la nota menor y cunatos eran hombre o mujeres
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-/* EJERCICOIO 5--.Construir el algoritmo que lea por teclado dos números,
-si el primero es mayor al segundo informar su suma y
-diferencia, en caso contrario, informar el producto y la
-división del primero respecto al segundo. */
-do{
-   let num1 = Number(prompt(`Digite el primer numero`));
-   let num2 = Number(prompt(`Digite el segundo numero`));
+/* EJERCICOIO 6--.Construir el algoritmo en Javascript para un programa
+para cualquier cantidad de estudiantes que lea el nombre,
+el sexo y la nota definitiva y halle al estudiante con la mayor
+nota y al estudiante con la menor nota y cuantos eran
+hombres y cuantos mujeres. */
+do {
+  let data_students = {
+    name: [],
+    sexo: [],
+    note: [],
+  };
+  let cant_men = 0;
+  let cant_women = 0;
 
-   if(num1 === num2){
-    alert(`Error ${num1} y ${num2} son numero iguales`)
-   }else{
-    (num1 > num2) 
-   ? alert(`el primer numero(${num1}) es mayor al segundo(${num2}), entonces su suma es ${num1 + num2}, con una diferencia de ${num1-num2}`)
-   : alert(`el segundo numero(${num2}) es mayor al primero(${num1}), entonces su producto es ${num2 * num1}, y la divicion de ${num2}/${num1} es de ${num2/num1}`)
+  let cant_students = Number(prompt(`Digite la cantidad de estudiantes que va a ingresar`));
+  save_students(cant_students);
+  alert(`El estudiantes con la MAYOR nota definitva es ${MaxNote_MinNote(data_students)}\n
+De los ${cant_students} estudiantes\n${cant_men} son hombres.\n${cant_women} son mujeres`);
+  
+  function save_students(estudiantes) {
+    for (let i = 1; i <= estudiantes; i++) {
+      name_student = prompt(`Digite el nombre del estudinate ${i}`);
+      sexo_student = Number(prompt(`Digite el sexo del estudiante ${i}\n1.Hombre\n2.Mujer`));
+      note_student = Number(prompt(`Digite la nota final del estudiante ${i}`));
 
-   }
-}while(confirm("¿Desea ingresar otros numeros?"))
+      data_students.name.push(name_student);
+      data_students.note.push(note_student);
 
+      (sexo_student === 1) ? (cant_men += 1) :(cant_women += 1)
+    }
+  }
 
+  function MaxNote_MinNote(data) {
+    let max_note = Math.max(...data.note);
+    let min_note = Math.min(...data.note);
+    let position_max_note = data.note.indexOf(max_note);
+    let position_min_note = data.note.indexOf(min_note);
+
+    return `${data.name[position_max_note]} con ${data.note[position_max_note]}\nEl estudiantes con la MENOR nota definitva es ${data.name[position_min_note]} con ${data.note[position_min_note]}`;
+  }
+} while (confirm("¿Desea ingresar otros estudiantes?"));
