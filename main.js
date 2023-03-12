ace67f81f1c170fe7dacae3bbbac4ab73d6332e4
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-12 10:52:29 -0500

    feat(Taller2-Javascript): :rocket: Ejercicio 4
    Solucion al ejercicio 4 del segundo  taller de javascript. Ingresamos x cantidad de personas y hallamos cual es la mayor en edad
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-/* EJERCICOIO 3--. Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente. */
+/* EJERCICOIO 4--. Construir el algoritmo que solicite el nombre y edad de 3
+personas y determine el nombre de la persona con mayor edad. */
 do{
-let resistencia = Number(prompt("Ingrese la resistencia (Ohmios)"));
-let intencidad = Number(prompt("Ingrese la intencidad (Amperios)"));
-let voltaje = resistencia * intencidad
+    let persons = {
+        name:[],
+        age: []
+    }
 
-alert(`El voltaje del circuito con una resistencia de ${resistencia}(Ω) y una intencidad de ${intencidad}(A) es: ${voltaje}(V)`)
-}while(confirm("¿Desea ingresar otro numero?"))
+    let cant_persons = Number(prompt("Cuantas va personas va a ingresar"));
+    save_person(cant_persons);
+    alert(`La persona mayor es ${result()}`)
+
+    function save_person(cantidad) {
+        for (let i = 1; i <= cantidad; i++) {
+            let name_person = prompt(`Digite el Nombre de la persona ${i}`)
+            let age_person = Number(prompt(`Digite la Edad de la persona ${i}`))
+            
+            persons.name.push(name_person);
+            persons.age.push(age_person)
+        }
+    }
+
+    function result(){
+        let age_max = Math.max(...persons.age);
+        let position = persons.age.indexOf(age_max)
+        
+        return `${persons.name[position]} con una edad de ${persons.age[position]} años`
+    }
+
+}while(confirm("¿Desea volver a ingresar mas personas?"))
 
 
