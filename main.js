d21b9bec263a6f3ea3699f121491a88aa02ba3c1
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-03-14 08:38:36 -0500

    feat(Taller2-Javascript): :rocket: Ejercicio 10
    Solucion al ejercicio 10 del segundo  taller de javascript. ingresamos x cantidad de numero y resolvemos algunas operaciones
    
    by:Carlos Villafrades

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
-/* EJERCICOIO 9--.N atletas han pasado a finales en salto triple en los juegos
-olímpicos femenino de 2022. Diseñe un programa que pida por
-teclado los nombres de cada atleta finalista y a su vez, sus
-marcas del salto en metros. Informar el nombre de la atleta
-campeona que se quede con la medalla de oro y si rompió
-récord, reportar el pago que será de 500 millones. El récord
-esta en 15,50 metros. */
+/* EJERCICOIO 10--.Desarrolle un programa cíclico que capture un dato
+numérico cada vez, y los vaya acumulando. El programa se
+detiene cuando el usuario digita un cero. El programa debe
+mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+VALOR Y MENOR VALOR.*/
 do {
-    data_athletes = {
-        name:[],
-        marca:[]
-    };
-    let record = 15.50;
+    let data_numbers = [];
+    do{
+        data_numbers.push(Number(prompt(`Digite un numero`)));
+        let sum_nums = 0;
+        let promedio;
 
-    let cant_athletes = Number(prompt(`Digite cuantos atletas pasaron a la final`));
-    save_athletes(cant_athletes);
-    alert(`La campeona, medalla de oro de los juegos olimpicos en la modalidad salto triple es ${calculate_winner(data_athletes)}`)
+        data_numbers.forEach(element => {
+            sum_nums += element
+            promedio = sum_nums / data_numbers.length
+        });
 
-    function save_athletes(cantidad) {
-        for (let i = 1; i <= cantidad; i++) {
-            let name_athlete = prompt(`Digite el nombre del atleta ${i}`);
-            let marca_athletes = Number(prompt(`Digite la marca del salto (m)`));
+        alert(`
+        \t${data_numbers}
+        Sumatoria: ${sum_nums}
+        Promedio: ${promedio}
+        Cantidad Valores: ${data_numbers.length}
+        Valor Mayor: ${Math.max(...data_numbers)}
+        Valor Menor: ${Math.min(...data_numbers)}`)
 
-            data_athletes.name.push(name_athlete.toUpperCase());
-            data_athletes.marca.push(marca_athletes); 
-
-            if(marca_athletes > record){
-                alert(`Felicitaciones!!! ${name_athlete} acabas de romper el record anterior de ${record}m, te acaba de ganas 500 millones`);
-                record = marca_athletes
-            }          
-        }
-    }
-    function calculate_winner(data) {
-        let marca_winner = Math.max(...data.marca);
-        let position_winner = data.marca.indexOf(marca_winner);
-        
-        return `${data.name[position_winner]} con una marca de ${marca_winner}m`
-    }
- } while (confirm("¿Desea ingresar otra opcion?"));
+    }while(confirm("¿Agregar otro numero?"))
+ } while (confirm("Programa terminado. ¿Deseas volver a correr el programa?"));
